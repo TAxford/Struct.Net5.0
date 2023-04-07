e1111fa67034ecb6a81513701aa8c39f2daa4cc6
Timothy Axford <t.axford2@yahoo.com>
2023-04-07 10:04:53 -0400

    Gave values and wrote to the console explaining the first game

--- Struct.Net5.0/Program.cs
+++ Struct.Net5.0/Program.cs
@@ -16,6 +16,16 @@ namespace Struct.Net5._0
         static void Main(string[] args)
         {
             Game game1;
+
+            game1.name = "Pokemon Go";
+            game1.developer = "Niantic";
+            game1.rating = 3.5;
+            game1.releaseDate = "01/07/2016";
+
+            Console.WriteLine("Game 1's name is : {0}", game1.name);
+            Console.WriteLine("Game 1's developer is : {0}", game1.developer);
+            Console.WriteLine("Game 1's rating is : {0}", game1.rating);
+            Console.WriteLine("Game 1's release date is : {0}", game1.releaseDate);
         }
     }
 }
