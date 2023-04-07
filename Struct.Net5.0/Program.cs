b77a92e4e60554548d477781b75f0b58cbe4ee31
Timothy Axford <t.axford2@yahoo.com>
2023-04-07 10:00:35 -0400

    Created struct Game with properties

--- Struct.Net5.0/Program.cs
+++ Struct.Net5.0/Program.cs
@@ -4,6 +4,15 @@ namespace Struct.Net5._0
 {
     internal class Program
     {
+        struct Game
+        {
+            public string name;
+            public string developer;
+            public double rating;
+            public string releaseDate;
+        }
+
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
